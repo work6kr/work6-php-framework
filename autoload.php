181bf543a80a5a0ee06b08d61a506ca6f607bdde
Jaewon kim <cs@work6.kr>
2019-10-07 13:33:19 +0900

    [김재원] 설치 오류 메시지 추가

--- autoload.php
+++ autoload.php
@@ -1,7 +1,14 @@
 <?
 
-if(!file_exists('common/db.php')){
-    header('Location:install.php');
+if(!file_exists('./.htaccess')){
+    die('.htaccess 파일이 누락되었습니다.');
+    exit;
+}
+
+
+if(!file_exists('./common/db.php')){
+    //header('Location:install.php');
+    die('프로그램이 설치되어 있지 않습니다.<br><a href="/install.php">프로그램 설치하기</a>');
     exit;
 }
 
